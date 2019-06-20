f94aebeb6fde38e446f6cb676dea8558bb231bb6
anikabhandari16 <51975120+anikabhandari16@users.noreply.github.com>
2019-06-20 09:57:13 -0700

    added js to calculator

---
+++ script.js
@@ -0,0 +1,45 @@
+function sumNumbers(){
+  var number_one = 
+    document.getElementById("numberOne").value
+
+var number_two = 
+    document. getElementById("numberTwo").value
+
+var answer = 
+(parseInt(number_one)+parseInt(number_two))
+
+var showAnswer = document.getElementById('sum'). innerHTML = answer
+
+}
+    
+function sumSubtract(){
+  var number_one = 
+    document.getElementById("numberOne").value
+
+var number_two = 
+    document. getElementById("numberTwo").value
+
+var answer = 
+(parseInt(number_one)-parseInt(number_two))
+
+var showAnswer = document.getElementById('subtract').innerHTML = answer
+
+}
+    
+   
+   function sumMultiply(){
+  var number_one = 
+    document.getElementById("numberOne").value
+
+var number_two = 
+    document.getElementById("numberTwo").value
+
+var answer = 
+(parseInt(number_one)* parseInt(number_two))
+
+var showAnswer = document.getElementById("multiply").innerHTML = answer
+
+}
+    
+   
+   
